73164696df42aa9df9eb05c8d3f6982ff92a06ec
Jamie Kyle <me@thejameskyle.com>
2019-05-13 17:01:10 -0700

    fix example

--- example/index.tsx
+++ example/index.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react"
 import { createContainer } from "../src/unstated-next"
 import { render } from "react-dom"
 
-function useCounter() {
+function useCounter(initialState = 0) {
 	let [count, setCount] = useState(initialState)
 	let decrement = () => setCount(count - 1)
 	let increment = () => setCount(count + 1)
@@ -14,11 +14,11 @@ let Counter = createContainer(useCounter)
 function CounterDisplay() {
 	let counter = Counter.useContainer()
 	return (
-		<>
+		<div>
 			<button onClick={counter.decrement}>-</button>
 			<span>{counter.count}</span>
 			<button onClick={counter.increment}>+</button>
-		</>
+		</div>
 	)
 }
 
@@ -26,7 +26,7 @@ function App() {
 	return (
 		<Counter.Provider>
 			<CounterDisplay />
-			<Counter.Provider>
+			<Counter.Provider initialState={2}>
 				<div>
 					<div>
 						<CounterDisplay />
