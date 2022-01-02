a84bff394fa1ca079769804673a6d48114b75c2b
gideon-shore <jinyu@shore.network>
2022-01-02 20:57:35 +0800

    feat: Fix Type Error When useHook Function needs any required arguments

--- src/unstated-next.tsx
+++ src/unstated-next.tsx
@@ -3,24 +3,26 @@ import React from "react"
 const EMPTY: unique symbol = Symbol()
 
 export type ContainerProviderProps<
-	State extends any
+	State extends any[]
 > = React.PropsWithChildren<{
-	initialState?: State
+	initialState: State
 }>
 
-export interface Container<Value, State extends any> {
+export type UseHookFn<Value, State extends any[]> = (...args: State) => Value
+
+export interface Container<Value, State extends any[]> {
 	Provider: React.ComponentType<ContainerProviderProps<State>>
 	useContainer: () => Value
 }
 
-export function createContainer<Value, State extends any>(
-	useHook: (initialState?: State) => Value,
+export function createContainer<Value, State extends any[]>(
+	useHook: UseHookFn<Value, State>,
 ): Container<Value, State> {
 	let Context = React.createContext<Value | typeof EMPTY>(EMPTY)
 
-	function Provider(props: ContainerProviderProps<State>) {
-		let value = useHook(props.initialState)
-		return <Context.Provider value={value}>{props.children}</Context.Provider>
+	function Provider({ initialState, children }: ContainerProviderProps<State>) {
+		const value = useHook(...initialState)
+		return <Context.Provider value={value}>{children}</Context.Provider>
 	}
 
 	function useContainer(): Value {
@@ -34,7 +36,7 @@ export function createContainer<Value, State extends any>(
 	return { Provider, useContainer }
 }
 
-export function useContainer<Value, State extends any>(
+export function useContainer<Value, State extends any[]>(
 	container: Container<Value, State>,
 ): Value {
 	return container.useContainer()
