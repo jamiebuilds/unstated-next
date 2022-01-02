6360215a30d07fa41f239e20a108300124a02d1b
gideon-shore <jinyu@shore.network>
2022-01-02 21:31:04 +0800

    feat: add Display Name for Context

--- src/unstated-next.tsx
+++ src/unstated-next.tsx
@@ -9,16 +9,23 @@ export type ContainerProviderProps<
 }>
 
 export type UseHookFn<Value, State extends any[]> = (...args: State) => Value
-
+export type ContainerProvider<State extends any[]> = React.ComponentType<
+	ContainerProviderProps<State>
+>
+export type UseContainer<Value> = () => Value
 export interface Container<Value, State extends any[]> {
-	Provider: React.ComponentType<ContainerProviderProps<State>>
-	useContainer: () => Value
+	Provider: ContainerProvider<State>
+	useContainer: UseContainer<Value>
 }
 
 export function createContainer<Value, State extends any[]>(
 	useHook: UseHookFn<Value, State>,
 ): Container<Value, State> {
-	let Context = React.createContext<Value | typeof EMPTY>(EMPTY)
+	const Context = React.createContext<Value | typeof EMPTY>(EMPTY)
+	const hookName = useHook.name
+		? `${useHook.name.charAt(0).toUpperCase()}${useHook.name.slice(1)}`
+		: "UnstatedNext"
+	Context.displayName = `Context${hookName}`
 
 	function Provider({ initialState, children }: ContainerProviderProps<State>) {
 		const value = useHook(...initialState)
@@ -26,14 +33,17 @@ export function createContainer<Value, State extends any[]>(
 	}
 
 	function useContainer(): Value {
-		let value = React.useContext(Context)
+		const value = React.useContext(Context)
 		if (value === EMPTY) {
 			throw new Error("Component must be wrapped with <Container.Provider>")
 		}
 		return value
 	}
 
-	return { Provider, useContainer }
+	return {
+		Provider,
+		useContainer,
+	}
 }
 
 export function useContainer<Value, State extends any[]>(
